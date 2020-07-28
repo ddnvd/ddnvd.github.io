794cdd3cc0d638b74f4c172a3eb7723d3807d84e
ddnvd <danilada22@gmail.com>
2020-07-28 17:08:33 +0700

    ygyg

---
+++ harmonica/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1595930860,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/note-sheet0.png",
+		"images/enter-sheet0.png",
+		"images/enter-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}

371d3b43b07ba1e8941610688a572382d1fc41a5
ddnvd <danilada22@gmail.com>
2021-02-18 15:15:55 +0300

    sstvideo

---
+++ sstvideo-jc/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1613650536,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/animation-sheet0.png",
+		"images/black_block-sheet0.png",
+		"images/sstvideo-sheet0.png",
+		"images/pomehi-sheet0.png",
+		"images/pomehi-sheet1.png",
+		"images/pomehi-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
